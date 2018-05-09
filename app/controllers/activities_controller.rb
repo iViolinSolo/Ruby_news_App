038a5cb2255882d9303a49ba8c1565c5a7ebbcea
Minghao Xu <i.violinsolo@gmail.com>
2018-05-09 11:13:11 +0800

    增加了ｓｈｏｗ

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,13 +1,29 @@
 class ActivitiesController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
   def new
   end
 
   def create
     # render plain: params[:activity].inspect
-    @activity = Activity.new(params[:activity])
+    # @activity = Activity.new(params[:activity])
+    @activity = Activity.new(activity_params)
     puts @activity
 
     @activity.save
     redirect_to @activity
   end
+
+  private
+
+    def activity_params
+      params.require(:activity).permit(:title, :content)
+    end
+
 end
